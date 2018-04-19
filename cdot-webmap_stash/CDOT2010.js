3214c67ab3ffe32d9356b9a1b40a8a24c4112f4b
David Himot <35549276+davidhimot@users.noreply.github.com>
2018-04-19 09:32:02 -0600

    20180419
    CDOT map with Widgets(Home, FullExtent)

--- cdot-webmap_stash/CDOT2010.js
+++ cdot-webmap_stash/CDOT2010.js
@@ -1,67 +1,43 @@
 require([
     "esri/Map",
-    "esri/layers/FeatureLayer",
     "esri/views/MapView",
+    "esri/layers/FeatureLayer",
     "esri/widgets/Legend",
-    "esri/widgets/Home",
+    "esri/widgets/Home",    
     "esri/widgets/Fullscreen",
     "dojo/domReady!"
-], function(
-        Map,
-         FeatureLayer,
-         MapView,
-         Legend, Home, Fullscreen
-        ) {
+],
+        function (Map, MapView, FeatureLayer, Home, Fullscreen
+                  ) {
 
-    //--------------------------------------------------------------------------
-    //
-    //  Setup Map and View
-    //
-    //--------------------------------------------------------------------------
-
-   
-    var map = new Map({
-        basemap:"osm",
-        /*    portalItem: {
-                        id: "a8d243ec60e64ed29cfc5057403c3863"
-                    }, 
-                    /*   {
-          portalItem: {
+    var layer = new FeatureLayer({
+        portalItem: {
             id: "1c8788144bfe448aa3ab18f7109afd80"
+        },
+        definitionExpression: "accident_d > 0",
+        title: "Crash Date",
+        minScale: 72223.819286
+    });
 
-        },*/
-        //     layers: [layer]
+    var map = new Map({
+        basemap: "osm",
+        portalItem:{
+            id:"1c8788144bfe448aa3ab18f7109afd80"
+        },
+        layers:[layer]
     });
 
     var view = new MapView({
         map: map,
-        //   layers: [layer],
         container: "viewDiv",
-        center: [-105, 40.5],
+        center: [-104.74368,40.25293],
         zoom: 9,
         constraints: {
-            minScale:// 72223.819286
-            1444470.6385720
-            //This also has something to do with the legend.
+            snapToZoom: false,
+            minScale: 72223.819286
         },
-        // This ensures that when going fullscreen
-        // The top left corner of the view extent
-        // stays aligned with the top left corner
-        // of the view's container
         resizeAlign: "top-left"
     });
-     var layer = new FeatureLayer({
-        url: "https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/cdot_2010_crash_shp_1/FeatureServer",
-        /*portalItem: {
-            id: "1c8788144bfe448aa3ab18f7109afd80"
-          },*/
-        definitionExpression: "accident_d > 1/1/2010",
-        title: "Crash Incedents",
-        minScale://72223.819286
-        1444470.6385720
-        //What does this have to doe with the legend?
-    });
- //   map.add("layer");
 
     //--------------------------------------------------------------------------
     //
@@ -113,21 +89,20 @@ require([
     view.whenLayerView(layer).then(setupHoverTooltip);
 
     // Starts the application by visualizing year 1984
-    setMonth(1/1/2010);
-
+    //need to figure out if this is a variable or a datafield
+    setMonth(1);
     //--------------------------------------------------------------------------
     //
     //  Methods
     //
     //--------------------------------------------------------------------------
 
-
-    // * Sets the current visualized construction year.
-
+    /**
+       * Sets the current visualized construction year.
+       */
     function setMonth(value) {
-        sliderValue.innerHTML = Math.year(value);+
-            " %</span> of the votes separate the two candidates";
-        slider.value = Math.date(value);
+        sliderValue.innerHTML = Math.floor(value);
+        slider.value = Math.floor(value);
         layer.renderer = createRenderer(value);
     }
 
@@ -138,15 +113,14 @@ require([
        * year are visualized with a pink color. Buildings built within that
        * 20-year time frame are assigned a color interpolated between blue and pink.
        */
-
-    function createRenderer(setMonth) {
+    function createRenderer(month) {
         var opacityStops = [{
             opacity: 1,
-            value: "accident_d"
+            value: month
         },
                             {
                                 opacity: 0,
-                                value: "accident_d" + 1440
+                                value: month + 1
                             }];
 
         return {
@@ -167,25 +141,24 @@ require([
                 type: "color",
                 field: "accident_d",
                 legendOptions: {
-                    title: "Happened:"
+                    title: "Built:"
                 },
                 stops: [{
-                    value: accident_d,
+                    value: month,
                     color: "#0ff",
-                    label: "in " + Math.floor(accident_d)
+                    label: "in " + Math.floor(month)
                 }, {
                     value: month - 1,
                     color: "#f0f",
-                    label: "in " + (Math.floor(accident_d) - 2)
+                    label: "in " + (Math.floor(month) - 2)
                 }, {
                     value: month - 5,
                     color: "#404",
-                    label: "before " + (Math.floor(accident_d) - 5)
+                    label: "before " + (Math.floor(month) - 5)
                 }]
             }]
         };
     }
-
     /**
        * Sets up a moving tooltip that displays
        * the construction year of the hovered building.
@@ -222,7 +195,7 @@ require([
                     var screenPoint = hit.screenPoint;
 
                     highlight = layerview.highlight(graphic);
-                    tooltip.show(screenPoint, "Crash Date " + graphic.getAttribute(
+                    tooltip.show(screenPoint, "Occured on " + graphic.getAttribute(
                         "accident_d"));
                 } else {
                     tooltip.hide();
@@ -253,7 +226,6 @@ require([
         animation = null;
         playButton.classList.remove("toggled");
     }
-
     /**
        * Animates the color visual variable continously
        */
@@ -267,8 +239,8 @@ require([
             }
 
             value += 0.5;
-            if (value > 12/31/2010) {
-                value = 1/1/2010;
+            if (value > 12) {
+                value = 1;
             }
 
             setMonth(value);
@@ -350,3 +322,125 @@ require([
     }
 
 });
+
+
+var crash =
+    {
+        field:"LOC_3",
+        type: "picture-marker",
+        url: "https://png.icons8.com/ios/40/000000/crashed-car.png",
+        width: 7,
+        height: 7
+    };
+
+var killed =
+    {
+        field: "killed",
+        type: "picture-marker",
+        url: "https://png.icons8.com/ios/40/000000/self-destruct-button-filled.png",
+        width: 7,
+        height: 7
+    };
+
+var injured =
+    {
+        type:"picture-marker",
+        field:"injured",            url:"https://png.icons8.com/ios/50/000000/ambulance.png",
+        width:7,
+        height:7
+    };
+
+var defaultSym={
+    type: "picture-marker",
+    url:"style/icons8-crashed-car-24.png"};
+
+var symRenderer ={
+    type:"unique-value",
+    defaultSymbol: defaultSym,
+    defaultLable:"No Crash",
+    //field:"LOC_3",
+    field:"killed",
+    field2:"injured",
+    fieldDelimiter:", ",
+    uniqueValueInfos:[{
+        /*  value:"['*'], <1, <1",
+            symbol:crash
+
+    },{*/
+        value:">=01, null",
+
+        symbol:killed
+
+    },{
+        value:"null, >=01",
+        symbol:injured
+    }]
+};
+
+
+var opLayer = new ArcGISDynamicMapServiceLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/cdot_2010_crash_shp_1/FeatureServer");
+     opLayer.setVisibleLayers([0]);
+
+    var layerDefinitions = [];
+    layerDefinitions[0] = "accident_d";
+    opLayer.setLayerDefinitions(layerDefinitions);
+
+
+// Create the layer and set the popup
+var CDOT2010 = new  FeatureLayer({
+    url: "https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/cdot_2010_crash_shp_1/FeatureServer/0",
+    renderer: CDOT2010Renderer,
+    renderer: symRenderer,
+
+    outFields: ["LOC_3","CITY_NM","vehicles","killed","injured","accident_d"],
+    popupTemplate: {
+        title:"Crash Site",
+        content: "<b>Location:</b> {LOC_3}<br><b>City:</b> {CITY_NM}<br><b>Date of Crash:</b>{accident_d}<br><b>Number of Vehicles Involved:</b>{vehicles}<br><b>Crash Result in Injury:</b> {injured} <br><b>Crash Result in Death:</b> {killed} <br>"
+    }
+});
+map.add(CDOT2010)
+};
+
+
+//map.add(CDOT2010)
+    /*Add the layer
+    map.add([opLayer]);
+    map.on("layers-add-result", initSlider);
+
+    function initSlider() {
+        var timeSlider = new TimeSlider({
+            style: "width: 100%;"
+        }, dom.byId("timeSliderDiv"));
+        map.setTimeSlider(timeSlider);
+
+        var timeExtent = new TimeExtent();
+        timeExtent.startTime = new Date("1/1/2010 UTC");
+        timeExtent.endTime = new Date("12/31/2010 UTC");
+        timeSlider.setThumbCount(2);
+        timeSlider.createTimeStopsByTimeInterval(timeExtent, 2, "esriTimeUnitsYears");
+        timeSlider.setThumbIndexes([0,1]);
+        timeSlider.setThumbMovingRate(2000);
+        timeSlider.startup();
+
+        var labels = arrayUtils.map(timeSlider.timeStops, function(timeStop, i) { 
+            if ( i % 2 === 0 ) {
+                return timeStop.getUTCFullMonth(); 
+            } else {
+                return "";
+            }
+        }); 
+
+        timeSlider.setLabels(labels);
+
+        timeSlider.on("time-extent-change", function(evt) {
+            var startValString = evt.startTime.getUTCFullYear();
+            var endValString = evt.endTime.getUTCFullYear();
+            dom.byId("daterange").innerHTML = "<i>" + startValString + " and " + endValString  + "<\/i>";
+        });
+    } 
+        }
+        ]
+        }
+        }
+});
+*/
--- js/CDOT2010.js
+++ js/CDOT2010.js
@@ -1,154 +1,80 @@
 require([
     "esri/Map",
-    //  "esri/layers/ArcGISDynamicMapServiceLayer",
+    //"esri/layers/ArcGISDynamicMapServiceLayer",
     "esri/views/MapView",
     "esri/layers/FeatureLayer",
     //"esri/widgets/Legend",
-    // "esri/widgets/Home",
-    // "esri/widgets/Fullscreen",
+    "esri/widgets/Home",
+    "esri/widgets/Fullscreen",
     "dojo/domReady!"
 ], 
         function
         (
         Map, 
          MapView, 
-         FeatureLayer)
+         FeatureLayer,
          //  Legend, 
-         //   Home, 
-         //    Fullscreen)
-         // TimeExtent, 
-         // TimeSlider,
-         // arrayUtils, 
-         //dom
-         
-{
-         var map = new Map({
-         basemap: "osm", 
-         portalItem: {
-         id: "a8d243ec60e64ed29cfc5057403c3863" // This was created in the "Style a web map" and "Configure pop-ups" design labs
-         }
-         });
+         Home, 
+         Fullscreen)
+        // TimeExtent, 
+        // TimeSlider,
+        // arrayUtils, 
+        //dom
 
-        var view = new MapView({
+        {
+    var map = new Map({
+        basemap: "osm", 
+        portalItem: {
+            id: "a8d243ec60e64ed29cfc5057403c3863" // This was created in the "Style a web map" and "Configure pop-ups" design labs
+        }
+    });
+
+    var view = new MapView({
         container: "viewDiv",
         map: map,
         center: [-104.54368,40.25293],
-        zoom: 9
-        });
+        zoom: 9,
+        constraints: {
+            minScale:// 72223.819286
+            14444700.63857200},
+        resizeAlign: "top-left"
+    });
 
-var crash =
-    {
-        field:"LOC_3",
+    view.ui.empty("top-left");
+    view.ui.add(titleDiv, "top-left");
+    view.ui.add(new Home({
+        view: view
+    }), "top-left");
+    view.ui.add(new Fullscreen({
+        view: view,
+    }), "top-right");
+
+
+
+    var defaultSym={
         type: "picture-marker",
-        url: "https://png.icons8.com/ios/40/000000/crashed-car.png",
-        width: 7,
-        height: 7
+        url:"style/icons8-crashed-car-24.png"};
+
+    var symRenderer ={
+        type:"unique-value",
+        defaultSymbol: defaultSym,
+        defaultLable:"No Crash",
+
     };
 
-var killed =
-    {
-        field: "killed",
-        type: "picture-marker",
-        url: "https://png.icons8.com/ios/40/000000/self-destruct-button-filled.png",
-        width: 7,
-        height: 7
-    };
+    // Create the layer and set the popup
+    var CDOT2010 = new FeatureLayer({
+        url: "https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/cdot_2010_crash_shp_1/FeatureServer/0",
 
-var injured =
-    {
-        type:"picture-marker",
-        field:"injured",            url:"https://png.icons8.com/ios/50/000000/ambulance.png",
-        width:7,
-        height:7
-    };
+        renderer: symRenderer,
 
-var defaultSym={
-    type: "picture-marker",
-    url:"style/icons8-crashed-car-24.png"};
-
-var symRenderer ={
-    type:"unique-value",
-    defaultSymbol: defaultSym,
-    defaultLable:"No Crash",
-    //field:"LOC_3",
-    field:"killed",
-    field2:"injured",
-    fieldDelimiter:", ",
-    uniqueValueInfos:[{
-        /*  value:"['*'], <1, <1",
-            symbol:crash
-
-    },{*/
-        value:">=01, null",
-
-        symbol:killed
-
-    },{
-        value:"null, >=01",
-        symbol:injured
-    }]
-};
-
-
-/*  var opLayer = new ArcGISDynamicMapServiceLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/cdot_2010_crash_shp_1/FeatureServer");
-     opLayer.setVisibleLayers([0]);
-
-    var layerDefinitions = [];
-    layerDefinitions[0] = "accident_d";
-    opLayer.setLayerDefinitions(layerDefinitions);
-*/
-
-// Create the layer and set the popup
-var CDOT2010 = new FeatureLayer({
-    url: "https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/cdot_2010_crash_shp_1/FeatureServer/0",
-    // renderer: CDOT2010Renderer,
-    renderer: symRenderer,
-
-    outFields: ["LOC_3","CITY_NM","vehicles","killed","injured","accident_d"],
-    popupTemplate: {
-        title:"Crash Site",
-        content: "<b>Location:</b> {LOC_3}<br><b>City:</b> {CITY_NM}<br><b>Date of Crash:</b>{accident_d}<br><b>Number of Vehicles Involved:</b>{vehicles}<br><b>Crash Result in Injury:</b> {injured} <br><b>Crash Result in Death:</b> {killed} <br>"
-    }
-});
-
-map.add(CDOT2010)
-/*Add the layer
-    map.add([opLayer]);
-    map.on("layers-add-result", initSlider);
-
-    function initSlider() {
-        var timeSlider = new TimeSlider({
-            style: "width: 100%;"
-        }, dom.byId("timeSliderDiv"));
-        map.setTimeSlider(timeSlider);
-
-        var timeExtent = new TimeExtent();
-        timeExtent.startTime = new Date("1/1/2010 UTC");
-        timeExtent.endTime = new Date("12/31/2010 UTC");
-        timeSlider.setThumbCount(2);
-        timeSlider.createTimeStopsByTimeInterval(timeExtent, 2, "esriTimeUnitsYears");
-        timeSlider.setThumbIndexes([0,1]);
-        timeSlider.setThumbMovingRate(2000);
-        timeSlider.startup();
-
-        var labels = arrayUtils.map(timeSlider.timeStops, function(timeStop, i) { 
-            if ( i % 2 === 0 ) {
-                return timeStop.getUTCFullMonth(); 
-            } else {
-                return "";
-            }
-        }); 
-
-        timeSlider.setLabels(labels);
-
-        timeSlider.on("time-extent-change", function(evt) {
-            var startValString = evt.startTime.getUTCFullYear();
-            var endValString = evt.endTime.getUTCFullYear();
-            dom.byId("daterange").innerHTML = "<i>" + startValString + " and " + endValString  + "<\/i>";
-        });
-    } 
+        outFields: ["LOC_3","CITY_NM","vehicles","killed","injured","accident_d"],
+        popupTemplate: {
+            title:"Crash Site",
+            content: "<b>Location:</b> {LOC_3}<br><b>City:</b> {CITY_NM}<br><b>Date of Crash:</b>{month_CRSH}/{day_CRSH} <b>Hour:</b>{hour_CRSH}<br><b>Number of Vehicles Involved:</b>{vehicles}<br><b>Crash Result in Injury:</b> {injured} <br><b>Crash Result in Death:</b> {killed} <br>"
         }
-        ]
-        }
-        }*/
+    });
+
+    map.add(CDOT2010)
+
 });
